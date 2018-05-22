0eae0794ea4064cfaae44ebbe7215bd21ef8846e
Andrew Morgan <andrewmorgan@Macintosh-c42c0326fb3d.local>
2018-05-22 16:05:37 -0400

    Remove gulp script watching and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 var gulp     = require('gulp'),
-    sass    = require('gulp-sass'),
-    livereload  = require('gulp-livereload');
+    sass    = require('gulp-sass');
+    // livereload  = require('gulp-livereload');
 
 var config = {
-    scripts: [
-        // Custom Scripts
-        './wp-content/themes/wp-blank/js/scripts.js'
-    ]
+    // scripts: [
+    //     // Custom Scripts
+    //     './wp-content/themes/wp-blank/js/scripts.js'
+    // ]
 };
 
 gulp.task('sass', function () {
 	return gulp.src('./wp-content/themes/wp-blank/style.scss')
         .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(livereload())
+        // .pipe(livereload())
         .pipe(gulp.dest('./wp-content/themes/wp-blank/'));
 });
 
-gulp.task('scripts', function () {
-	return gulp.src(config.scripts)
-	.pipe(livereload())
-	.pipe(gulp.dest('./wp-content/themes/wp-blank/js/'));
-
-});
+// gulp.task('scripts', function () {
+// //return gulp.src(config.scripts)
+// 	// .pipe(livereload())
+// //	.pipe(gulp.dest('./wp-content/themes/wp-blank/js/'));
+//
+// });
 
 gulp.task('watch', function () {
-	livereload.listen(35729);
-	gulp.watch('**/*.php').on('change', function(file) {
-      livereload.changed(file.path);
-  	});
+	//livereload.listen(35729);
+	// gulp.watch('**/*.php').on('change', function(file) {
+  //     livereload.changed(file.path);
+  // 	});
 	gulp.watch('./wp-content/themes/wp-blank/*.scss', ['sass']);
-	gulp.watch('./wp-content/themes/wp-blank/js/*.js', ['scripts']);
+	// gulp.watch('./wp-content/themes/wp-blank/js/*.js', ['scripts']);
 });
 
-gulp.task('default', ['sass', 'scripts', 'watch']);
+gulp.task('default', ['sass',  'watch']);
